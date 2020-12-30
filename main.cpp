9be881d7d7a22d435f83bf5d6112bcdcb6205a57
voidhoge <coilgunrailgun@gmail.com>
2020-12-31 00:31:56 +0900

    ファイル名がない場合にabortするようにした

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "mandelbrot.hpp"
 #include "bmp.hpp"
+#include <cstdlib>
 
 int main(int argc, char const *argv[]) {
 	if (argc != 2) {
 		std::cerr << "Enter the file name after the executable file." << '\n';
+		std::abort();
 	}
 	// diverge
 	std::vector<pixel> gradation_waypoint;
@@ -19,7 +21,7 @@ int main(int argc, char const *argv[]) {
 	bmp_write file(argv[1]);
 	complex_t center(-0.6428f, 0.4507f);
 	complex_t range((hogefloat_t)16/60, (hogefloat_t)9/60);
-	grid data = mandelbrot_bmp_multithread(center, range, gradation_waypoint, 1920*2, 1080*2, 16);
+	grid data = mandelbrot_bmp_multithread(center, range, gradation_waypoint, 1920*2, 1080*2, 8);
 	file.set_data(data);
 	file.write();
 	return 0;
