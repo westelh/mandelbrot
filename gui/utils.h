6a81b3395daaf6120edfc5473ca544558aa4ab56
westelh <westelh@gmail.com>
2021-02-17 11:40:09 +0900

    Update utils.h
    Mark pack_rgb() noexcept

--- gui/utils.h
+++ gui/utils.h
@@ -4,7 +4,7 @@
 #include <cstdint>
 
 // pack RGB as ARGB 32bit format
-constexpr std::uint32_t pack_rgb(std::uint8_t r, std::uint8_t g, std::uint8_t b) {
+constexpr std::uint32_t pack_rgb(std::uint8_t r, std::uint8_t g, std::uint8_t b) noexcept {
     uint32_t ret = 0xff;    // let alpha channel 100%
     ret = ret << 8;
     ret = ret | r;
