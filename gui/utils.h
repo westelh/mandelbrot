2258194f573033a0b144903fc8f26176ef2c22d1
westelh <westelh@gmail.com>
2021-02-17 12:37:28 +0900

    Add pick()
    Add pick(color) to get each rgb parameter contained by variable

--- gui/utils.h
+++ gui/utils.h
@@ -15,4 +15,17 @@ constexpr std::uint32_t pack_rgb(std::uint8_t r, std::uint8_t g, std::uint8_t b)
     return ret;
 }
 
+enum class argb : std::uint8_t {
+    // offset
+    alpha = 24,
+    red   = 16,
+    green = 8,
+    blue  = 0,
+};
+
+template<argb Offset>
+constexpr std::uint8_t pick(std::uint32_t color) noexcept {
+    return color >> static_cast<std::uint32_t>(Offset);
+}
+
 #endif //MANDELBROT_UTILS_H
