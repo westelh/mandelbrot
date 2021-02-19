6cba51b12e461d1628030584db83f3f4ac432b64
westelh <westelh@gmail.com>
2021-02-19 22:05:33 +0900

    Update utils.h
    Add utility function

--- gui/utils.h
+++ gui/utils.h
@@ -2,6 +2,14 @@
 #define MANDELBROT_UTILS_H
 
 #include <cstdint>
+#include <span>
+#include "bmp.hpp"
+
+struct coord {
+    using type = std::uint16_t;
+    type x;
+    type y;
+};
 
 // pack RGB as ARGB 32bit format
 constexpr std::uint32_t pack_rgb(std::uint8_t r, std::uint8_t g, std::uint8_t b) noexcept {
@@ -27,6 +35,17 @@ template<argb Offset>
 constexpr std::uint8_t pick(std::uint32_t color) noexcept {
     auto array = reinterpret_cast<unsigned char *>(&color);
     return array[static_cast<int>(Offset)];
+}
+
+inline std::uint32_t pixel_to_uint32t(const pixel& pixel) {
+    return pack_rgb(pixel.r, pixel.g, pixel.b);
+}
+
+template<class OutputIterator>
+void grid_to_linear(const grid& from, OutputIterator to) {
+    for (const auto & i : from) {
+        std::transform(i.begin(), i.end(), to, pixel_to_uint32t);
+    }
 }
 
 #endif //MANDELBROT_UTILS_H
